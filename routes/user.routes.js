702df4c6b93e566cafd728f6a119eb46366cc4ba
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-06 11:27:37 +0530

    Update user.routes.js

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { userModel } = require("../models/User.model");
+const { userModel } = require("../models/user.model");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
 
