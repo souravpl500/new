d8f4b520062b13e32d972cdb8dbbcf2e72c07b7e
souravpl500 <111083486+souravpl500@users.noreply.github.com>
2023-03-06 11:01:52 +0530

    Create db.js

--- configs/db.js
+++ configs/db.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-mongoose.set("strictQuery", false);
 const connection = mongoose.connect(
   "mongodb+srv://souravpl500:Souravpl500@cluster0.hlt1sxm.mongodb.net/mock?retryWrites=true&w=majority"
 );
